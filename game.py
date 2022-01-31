fe7b07b638b1b21f7bcce56c6629ecc2a7dcfb80
ca863 <ca863@georgetown.edu>
2022-01-31 17:41:15 -0500

    Computer choice requirement finished

--- game.py
+++ game.py
@@ -1,16 +1,13 @@
+import random
 
-
-
-#
-# todo: write some Python code here to satisfy the exercise objectives
-# ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
-#
-
+#Welcome player
+print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
 
 #ask for user input
+user_input = input("Please choose one of: 'rock', 'paper', 'scissors':") #asks for user input
+user_input = user_input.lower() #converts to lower case to encompass all combinations
 
-from random import choices
-user_input = input("Please choose one of: 'rock', 'paper', 'scissors':").lower #asks for user input & converts to lower case to encompass all combinations
+#validate user input
 valid_choices = ["rock","paper","scissors"] #lists out the possible choices
 if user_input not in valid_choices:
     print("That is not a valid choice, please try again!")
@@ -19,18 +16,19 @@ else:
     print("You chose:", u)
 
 
-
-
-
-#validate user input
-
-
-
+    #figure out how to do loops and 
 
 #computer choice
 
+computer_input = random.choice(valid_choices) #computer randomnly picks rock, paper or scissors
+print("The computer chose: ", computer_input) #prints out the computer's choice
 
 #determine the winner
-
+if user_input == computer_input
+    print("It's a tie!")
+    elif
 
 #display the results
+
+#farewell message and option to play the game again?
+print("Thanks for playing. Please play again!")
