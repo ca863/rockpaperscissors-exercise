e9834b3853d48535aa93655a459788177e49cd9b
ca863 <ca863@georgetown.edu>
2022-01-31 17:53:53 -0500

    Winner determination using if statements

--- game.py
+++ game.py
@@ -1,9 +1,9 @@
 import random
 
-#Welcome player
+#Welcome player (finished)
 print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
 
-#ask for user input
+#ask for user input (finished)
 user_input = input("Please choose one of: 'rock', 'paper', 'scissors':") #asks for user input
 user_input = user_input.lower() #converts to lower case to encompass all combinations
 
@@ -18,15 +18,29 @@ else:
 
     #figure out how to do loops and 
 
-#computer choice
+#computer choice (finished)
 
 computer_input = random.choice(valid_choices) #computer randomnly picks rock, paper or scissors
 print("The computer chose: ", computer_input) #prints out the computer's choice
 
-#determine the winner
-if user_input == computer_input
-    print("It's a tie!")
-    elif
+#determine the winner, adapted from Slack
+if user_input == computer_input:
+    print("Both players played", user_choice, "It's a tie!")
+elif user_input == "paper":
+    if computer_input == "rock":
+        print("Paper covers rock. You won!")
+    else:
+        print("Scissors cuts paper. You lost! It's ok.")
+elif user_input == "scissors":
+    if computer_input == "paper":
+        print("Scissors cuts paper. You won!")
+    else:
+        print("Rock crushes scissors. You lost! It's ok.")
+elif user_input == "rock":
+    if computer_input == "scissors":
+        print("Rock crushes scissors. You won!")
+    else:
+        print("Paper covers rock You lost! It's ok.")
 
 #display the results
 
