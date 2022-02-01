7d8ff8eb8a52ca2207ca3ed1e6a6ba54d3beba45
ca863 <ca863@georgetown.edu>
2022-02-01 12:55:06 -0500

    Cleaning up the code

--- game.py
+++ game.py
@@ -9,24 +9,16 @@ user_input = user_input.lower() #converts to lower case to encompass all combina
 
 #validate user input
 valid_choices = ["rock","paper","scissors"] #lists out the possible choices
-##if user_input not in valid_choices:
-#    print("That is not a valid choice, please try again!")
-#    user_input = input("Please choose one of: 'rock', 'paper', 'scissors':").lower
-#else:
-#    print("You chose:", user_input)
 
 if user_input not in valid_choices:
     print("That is not a valid choice, please try again!")
     exit()
 
-
-
 #computer choice (finished)
-
 computer_input = random.choice(valid_choices) #computer randomnly picks rock, paper or scissors
 print("The computer chose:", computer_input) #prints out the computer's choice
 
-#determine the winner, adapted from Slack (finsihed)
+#determine the winner, adapted from Slack (finsihed) & display results
 if user_input == computer_input:
     print("Both players played", user_input, "It's a tie!")
 elif user_input == "paper":
@@ -45,15 +37,5 @@ elif user_input == "rock":
     else:
         print("Paper covers rock You lost! It's ok.")
 
-#display the results
-
-#farewell message and option to play the game again?
-#print("Thanks for playing. Please play again!")
-#user_play_again = input("Do you want to play again? Type 'yes' or 'no'")
-#user_play_again = user_play_again.lower #converts to lower case to encompass all combinations
-##if yes then replay else print farewwell message
-#if user_play_again == "yes":
-#    RESTART GAME #????         ·
-#else:
-#    print("See you soon!")
-#
+#farewell message
+print("Thank you for playing! Please play again!")
